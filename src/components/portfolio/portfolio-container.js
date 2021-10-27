9f8e59c41f1305ed8ecd95bbbeb93dda99494943
JapanCodeman <ryansurdick@MacBook-Pro.local>
2021-10-28 06:23:14 +0900

    Small indentation fix

--- src/components/portfolio/portfolio-container.js
+++ src/components/portfolio/portfolio-container.js
@@ -18,12 +18,12 @@ export default class PortfolioContainer extends Component {
   }
 
   render() {
-      return (
-        <div>
-          <h2>Portfolio items go here...</h2>
+    return (
+      <div>
+        <h2>Portfolio items go here...</h2>
 
-          {this.portfolioItems()}
-       </div>
+        {this.portfolioItems()}
+      </div>
     )
   }
 }
