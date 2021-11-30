08125b97c166d2cf6a56a7e27f51be2ec0b85e5f
JapanCodeman <ryansurdick@MacBook-Pro.mshome.net>
2021-11-30 20:13:26 +0900

    finished off edit mode features, incorporated update elements utilizing the API endpoint, authorization rules for blog detail, page styles for react portfolio, contact page styles, began portfolio detail component

--- Basics/src/components/pages/about.js
+++ Basics/src/components/pages/about.js
@@ -1,9 +1,37 @@
-import React from 'react';
+import React from "react";
+import heroImage from "../../../static/assets/images/hero_images/coding_screen.jpg";
 
-export default function() {
+export default function () {
   return (
-    <div>
-      About
+    <div className="content-page-wrapper">
+      <div
+        className="left-column"
+        style={{
+          background: "url(" + heroImage + ") no-repeat",
+          backgroundSize: "cover",
+          backgroundPosition: "center",
+        }}
+      />
+      <div className="right-column">
+        <h1>Ryan Surdick</h1>
+        <h2>A new recruit to the development world!</h2>
+        <p>
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+          eiusmod tempor incididunt ut labore et dolore magna aliqua. Quis risus
+          sed vulputate odio ut enim blandit. Ultrices eros in cursus turpis
+          massa tincidunt dui. Et netus et malesuada fames ac turpis egestas sed
+          tempus. Quisque egestas diam in arcu cursus. Velit ut tortor pretium
+          viverra suspendisse. Orci eu lobortis elementum nibh tellus molestie.
+          A condimentum vitae sapien pellentesque habitant. Semper viverra nam
+          libero justo laoreet sit amet cursus sit. Quam viverra orci sagittis
+          eu volutpat odio facilisis mauris sit. Id nibh tortor id aliquet.
+          Massa eget egestas purus viverra accumsan. Orci ac auctor augue mauris
+          augue neque gravida in fermentum. Risus at ultrices mi tempus
+          imperdiet. Nulla facilisi cras fermentum odio eu feugiat pretium nibh
+          ipsum. Nunc mattis enim ut tellus elementum sagittis vitae. Sapien
+          faucibus et molestie ac. 
+        </p>
+      </div>
     </div>
   );
 }
--- Basics/src/components/portfolio/portfolio-item.js
+++ Basics/src/components/portfolio/portfolio-item.js
@@ -1,45 +1,50 @@
 import React, { Component } from "react";
-import { Link } from 'react-router-dom';
+import { Link } from "react-router-dom";
 
 export default class PorfolioItem extends Component {
   constructor(props) {
     super(props);
-  
+
     this.state = {
-      porfolioItemClass: ""
+      porfolioItemClass: "",
     };
   }
 
   handleMouseEnter() {
-    this.setState({porfolioItemClass: 'img-blur'});
+    this.setState({ porfolioItemClass: "img-blur" });
   }
 
   handleMouseLeave() {
-    this.setState({porfolioItemClass: ""});
+    this.setState({ porfolioItemClass: "" });
   }
 
   render() {
     const { id, description, thumb_image_url, logo_url } = this.props.item;
-  return (
-    <div className="portfolio-item-wrapper"
-    onMouseEnter = {() => this.handleMouseEnter()}
-    onMouseLeave = {() => this.handleMouseLeave()}
-    >
-      <div 
-        className={"portfolio-img-background " + this.state.porfolioItemClass}
-        style={{
-          backgroundImage: "url(" + thumb_image_url + ")"
-        }}
-      />
+    return (
+      <Link to={`/portfolio/${id}`}>
+        <div
+          className="portfolio-item-wrapper"
+          onMouseEnter={() => this.handleMouseEnter()}
+          onMouseLeave={() => this.handleMouseLeave()}
+        >
+          <div
+            className={
+              "portfolio-img-background " + this.state.porfolioItemClass
+            }
+            style={{
+              backgroundImage: "url(" + thumb_image_url + ")",
+            }}
+          />
 
-      <div className="img-text-wrapper">
-        <div className="logo-wrapper">
-          <img src={logo_url} />
+          <div className="img-text-wrapper">
+            <div className="logo-wrapper">
+              <img src={logo_url} />
+            </div>
+
+            <div className="subtitle">{description}</div>
+          </div>
         </div>
-
-        <div className="subtitle">{description}</div>
-      </div>
-    </div>
-  );
+      </Link>
+    );
   }
 }
